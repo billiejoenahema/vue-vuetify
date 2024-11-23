925cf30633a2251005f837c4a45fc012bed13159
billiejoenahema <billiejoenahema@gmail.com>
2024-11-23 10:07:37 +0900

    feat: add settings to vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
+    define: {
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    },
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+        hmr: {
+            host: "localhost",
+        },
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./resources/js/"),
+        },
+    },
 });
